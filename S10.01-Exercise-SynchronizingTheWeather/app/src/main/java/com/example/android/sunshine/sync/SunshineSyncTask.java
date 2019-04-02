222675f232b15c80c763d32a959580675c791ea0
GretaGri <greta.klimaite@gmail.com>
2019-04-02 22:56:53 +0300

    Done part of TODO's in Lesson 10 exercise 01 - adding Service and synchronizing the weather.

--- S10.01-Exercise-SynchronizingTheWeather/app/src/main/java/com/example/android/sunshine/sync/SunshineSyncTask.java
+++ S10.01-Exercise-SynchronizingTheWeather/app/src/main/java/com/example/android/sunshine/sync/SunshineSyncTask.java
@@ -1,4 +1,15 @@
-//  TODO (1) Create a class called SunshineSyncTask
-//  TODO (2) Within SunshineSyncTask, create a synchronized public static void method called syncWeather
-//      TODO (3) Within syncWeather, fetch new weather data
-//      TODO (4) If we have valid results, delete the old data and insert the new
+package com.example.android.sunshine.sync;
+
+import com.example.android.sunshine.utilities.NetworkUtils;
+
+// Done (1) Create a class called SunshineSyncTask
+ public class SunshineSyncTask {
+    // Done (2) Within SunshineSyncTask, create a synchronized public static void method called syncWeather
+     public static void syncWeather (){
+         // Done (3) Within syncWeather, fetch new weather data
+
+        // Done (4) If we have valid results, delete the old data and insert the new
+
+     }
+}
+
